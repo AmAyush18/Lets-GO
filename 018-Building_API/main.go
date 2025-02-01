d8ca55938aa8b3a8dcd5b9e55813fe4f063657e5
Ayush Dwivedi <amayush0818@gmail.com>
2025-02-02 03:35:05 +0530

    Creating Routes and testing

--- 018-Building_API/main.go
+++ 018-Building_API/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -34,7 +35,23 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
+	fmt.Println("API - CodeTherapist.edu")
 
+	r := mux.NewRouter()
+
+	// seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "ReactJS", CoursePrice: 199, Author: &Author{Fullname: "Ayush", Website: "codetheapist.edu"}})
+	courses = append(courses, Course{CourseId: "4", CourseName: "Typescript", CoursePrice: 79, Author: &Author{Fullname: "Ayush", Website: "codetheapist.edu"}})
+
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controllers - file
@@ -81,10 +98,12 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	// what about - {}
 
 	var course Course
-	_ := json.NewDecoder(r.Body).Decode(&course)
+
+	json.NewDecoder(r.Body).Decode(&course)
 
 	if course.IsEmpty() {
 		json.NewEncoder(w).Encode("No data in JSON")
+		return
 	}
 
 	// generate unique id, string
@@ -117,6 +136,7 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 			courses = append(courses, course)
 
 			json.NewEncoder(w).Encode(course)
+			return
 		}
 	}
 	json.NewEncoder(w).Encode("Course not found")
@@ -134,7 +154,7 @@ func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
 		if course.CourseId == params["id"] {
 			courses = append(courses[:index], courses[index+1:]...)
 			json.NewEncoder(w).Encode("Course Deleted successfully")
-			break
+			return
 		}
 	}
 	json.NewEncoder(w).Encode("Course not found")
