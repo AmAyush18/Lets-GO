8acf7b003cce1524aac1906cda0c0214125dec80
Ayush Dwivedi <amayush0818@gmail.com>
2025-02-02 03:00:40 +0530

    Update Course Controller

--- 018-Building_API/main.go
+++ 018-Building_API/main.go
@@ -54,7 +54,6 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 
 func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get one course")
-
 	w.Header().Set("Content-Type", "application/json")
 
 	// grab id from request
@@ -72,7 +71,6 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 
 func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Create one course")
-
 	w.Header().Set("Content-Type", "application/json")
 
 	// what if: body is empty
@@ -98,4 +96,29 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(course)
 	return
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// first - grab id from req
+	params := mux.Vars(r)
+
+	// loop, id, remove, add with my ID
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+
+			json.NewEncoder(w).Encode(course)
+		}
+	}
+	json.NewEncoder(w).Encode("Course not found")
 }
